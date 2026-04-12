0bc1809a375ac4d9d8811210a9c60b4c97ecb100
Eugen Ganscha <eganscha@gmail.com>
2026-04-12 17:55:08 +0200

    Pikachu (First Commit): set up initial backend project structure

---
+++ src/main/java/de/mudkip/mirabackend/MirabackendApplication.java
@@ -0,0 +1,13 @@
+package de.mudkip.mirabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirabackendApplication.class, args);
+	}
+
+}
