99f3ecfacc8b2e98770acd2145376d0e82b74887
Eugen Ganscha <eganscha@gmail.com>
2026-04-19 08:40:29 +0200

    Using BCryptPasswordEncoder for Hashing the passwords instead of NoOp

--- src/main/java/de/mudkip/mirabackend/security/security/config/SecurityConfig.java
+++ src/main/java/de/mudkip/mirabackend/security/security/config/SecurityConfig.java
@@ -4,10 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -27,6 +25,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
